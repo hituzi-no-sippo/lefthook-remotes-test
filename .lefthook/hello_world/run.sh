cd2c5cce8efb6d885bd0bc189631a7766dac3794
hituzi no sippo <dev@hituzi-no-sippo.me>
2024-01-26 16:12:57 +0900

    add a lefthook configuration

---
+++ .lefthook/hello_world/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# @(#) v0.1.0 2023-11-09T07:45:45+0900
+# @(#) Copyright (C) 2023 hituzi no sippo
+# @(#) LICENSE: MIT-0 (https://choosealicense.com/licenses/mit-0/)
+
+echo 'hello world'
